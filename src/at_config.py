18335b795a85b2053e01fe77a5ffe56576f818e7
Anton Köhler <54813831+AKoehler95@users.noreply.github.com>
2025-07-03 15:51:59 +0200

    initial commit

---
+++ src/at_config.py
@@ -0,0 +1,74 @@
+import json
+from at_element import ATElement, ATElementType
+import math
+
+class ATConfiguration:
+    def __init__(self):
+        self.alpha_l = 2.0
+        self.alpha_t = 0.2
+        self.beta = 1 / (2 * self.alpha_l)
+        self.ca = 8.0
+        self.gamma = 3.5
+        self.dom_xmin = 0.0
+        self.dom_xmax = 600.0
+        self.dom_ymin = -10.0
+        self.dom_ymax = 40.0
+        self.dom_inc = 0.5
+        self.num_cp = 100
+        self.num_terms = 7
+        self.elements = []
+        self.orientation = "horizontal"
+        self.plot_aspect = "scaled"
+
+    @staticmethod
+    def from_json(filename):
+        with open(filename, 'r') as f:
+            data = json.load(f)
+
+        config = ATConfiguration()
+        config.alpha_l = data.get('alpha_l', config.alpha_l)
+        config.alpha_t = data.get('alpha_t', config.alpha_t)
+        config.beta = 1 / (2 * config.alpha_l)
+        config.ca = data.get('ca', config.ca)
+        config.gamma = data.get('gamma', config.gamma)
+        config.dom_xmin = data.get('dom_xmin', config.dom_xmin)
+        config.dom_xmax = data.get('dom_xmax', config.dom_xmax)
+        config.dom_ymin = data.get('dom_ymin', config.dom_ymin)
+        config.dom_ymax = data.get('dom_ymax', config.dom_ymax)
+        config.dom_inc = data.get('dom_inc', config.dom_inc)
+        config.num_cp = data.get('num_cp', config.num_cp)
+        config.num_terms = data.get('num_terms', config.num_terms)
+        config.orientation = data.get('orientation', config.orientation)
+        config.plot_aspect = data.get('plot_aspect', config.plot_aspect)
+
+        for i, elem_data in enumerate(data.get('elements', [])):
+            kind = elem_data['kind'].lower()
+            if kind == 'circle':
+                elem = ATElement(
+                    kind=ATElementType.Circle,
+                    x=elem_data['x'],
+                    y=elem_data['y'],
+                    c=elem_data['c'],
+                    r=elem_data['r']
+                )
+            elif kind == 'line':
+                theta_deg = elem_data.get('theta',90)
+                elem = ATElement(
+                    kind=ATElementType.Line,
+                    x=elem_data['x'],
+                    y=elem_data['y'],
+                    c=elem_data['c'],
+                    r=elem_data['l']/2.0,  #radius
+                    theta=math.radians(theta_deg)  # default vertical
+                )
+            else:
+                raise ValueError(f"Unknown element kind '{elem_data['kind']}'")
+
+            elem.label = elem_data.get('label', f"Element {i + 1}")
+            elem.id = elem_data.get('id', f"source_{len(config.elements)}")
+            config.elements.append(elem)
+        return config
+
+    def annotate_elements_on_plot(self, ax):
+        for elem in self.elements:
+            ax.text(elem.x, elem.y, elem.label, fontsize=14, ha='center', va='center', color='black', bbox=dict(facecolor='white', alpha=0.5))
---
+++ src/at_element.py
@@ -0,0 +1,71 @@
+import math
+import numpy as np
+from enum import Enum
+from mathieu_functions_OG import Mathieu
+
+class ATElementType(Enum):
+    Circle = 0
+    Line = 1
+
+class ATElement:
+    def __init__(self, kind: ATElementType, x: float, y: float, c: float, r = 1.0, theta: float = math.pi/2):
+        self.kind = kind
+        self.x = x
+        self.y = y
+        self.c = c
+        self.r = r
+        self.theta = theta
+        self.d = None
+        self.q = None
+        self.outline = []
+        self.m = None
+
+    def calc_d_q(self, alpha_t, alpha_l, beta):
+        r = self.r
+        a = self.r
+        self.d = math.sqrt((a * math.sqrt(alpha_l / alpha_t))**2 - a**2)
+        self.q = (self.d ** 2 * beta ** 2) / 4
+        self.m = Mathieu(self.q)
+
+    def set_outline(self, num_cp):
+        if self.kind == ATElementType.Circle:
+            phi = np.linspace(0, 2 * math.pi, num_cp, endpoint=False)
+            self.outline = [
+                (self.x + self.r * math.cos(p),
+                 self.y + self.r * math.sin(p))
+                for p in phi
+            ]
+
+        elif self.kind == ATElementType.Line:
+            half_len = self.r
+            t_vals = np.linspace(-half_len, half_len, num_cp)
+            self.outline = [
+                (self.x + t * math.cos(self.theta),
+                 self.y + t * math.sin(self.theta))
+                for t in t_vals
+            ]
+        else:
+            raise ValueError(f"Unknown element kind: {self.kind}")
+
+
+    def uv(self, x, y, alpha_l, alpha_t):
+        d = self.d
+        Y = math.sqrt(alpha_l / alpha_t) * y
+        B = x**2 + Y**2 - d**2
+        sqrt_term = math.sqrt(B**2 + 4 * d**2 * x**2)
+        p = (-B + sqrt_term) / (2 * d**2)
+        q = (-B - sqrt_term) / (2 * d**2)
+
+        psi_0 = math.asin(min(max(math.sqrt(p), -1.0), 1.0))
+        if Y >= 0 and x >= 0:
+            psi = psi_0
+        elif Y < 0 and x >= 0:
+            psi = math.pi - psi_0
+        elif Y <= 0 and x < 0:
+            psi = math.pi + psi_0
+        else:
+            psi = 2 * math.pi - psi_0
+
+        eta = 0.5 * math.log(1 - 2 * q + 2 * math.sqrt(q**2 - q))
+        return eta, psi
+
